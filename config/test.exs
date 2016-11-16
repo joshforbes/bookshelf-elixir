dbf38bfbf1a2065522ec61d22910212bd41d6534
Josh Forbes <jforbes@kimmel.com>
2016-11-15 21:39:58 -0500

    Adding database config info for test database.

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :bookshelf, Bookshelf.Repo,
   adapter: Ecto.Adapters.MySQL,
-  username: "root",
-  password: "",
+  username: "homestead",
+  password: "secret",
   database: "bookshelf_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
