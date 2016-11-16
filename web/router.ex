a948665264129ca71a3288da5def2767e393b4b3
Josh Forbes <jforbes@kimmel.com>
2016-11-15 21:54:25 -0500

    Adding a route for the books index.

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,9 @@ defmodule Bookshelf.Router do
   scope "/", Bookshelf do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    scope "Books" do
+      get "/", BookController, :index
+    end
   end
 
   # Other scopes may use custom stacks.
