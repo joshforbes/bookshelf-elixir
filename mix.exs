b15db008bbe1820eefe41bb7256c5e6fd5d9326c
Josh Forbes <jforbes@kimmel.com>
2016-11-15 21:25:56 -0500

    Require ex_machina for factories.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bookshelf.Mixfile do
   def application do
     [mod: {Bookshelf, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex]]
+                    :phoenix_ecto, :mariaex, :ex_machina]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,8 @@ defmodule Bookshelf.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ex_machina, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
