d938e2b325bdc42f1d2e6383fd195c6addfc7d54
Josh Forbes <jforbes@kimmel.com>
2016-11-15 21:38:17 -0500

    Test for the index endpoint, to display all books.

---
+++ test/controllers/book_controller_test.ex
@@ -0,0 +1,21 @@
+defmodule Bookshelf.BookControllerTest do
+  use Bookshelf.ConnCase
+
+  test "#index renders a list of books" do
+    conn = build_conn()
+    book = insert(:book)
+
+    conn = get conn, todo_path(conn, :index)
+
+    assert json_response(conn, 200) == %{
+      "books" => [%{
+        "title" => book.title,
+        "author" => book.description,
+        'cover_path' => book.cover_path,
+        'file_path' => book.file_path,
+        "inserted_at" => Ecto.DateTime.to_iso8601(book.inserted_at),
+        "updated_at" => Ecto.DateTime.to_iso8601(book.updated_at)
+      }]
+    }
+  end
+end
