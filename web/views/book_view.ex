66371672057c29559e5e4aee99b596fbdd3dbbed
Josh Forbes <jforbes@kimmel.com>
2016-11-15 22:10:11 -0500

    Serializing the book model to json.

---
+++ web/views/book_view.ex
@@ -0,0 +1,20 @@
+defmodule Bookshelf.BookView do
+  use Bookshelf.Web, :view
+
+  def render("index.json", %{books: books}) do
+    %{
+      books: Enum.map(books, &book_json/1)
+    }
+  end
+  
+  def book_json(book) do
+    %{
+      title: book.title,
+      author: book.author,
+      cover_path: book.cover_path,
+      file_path: book.file_path,
+      inserted_at: book.inserted_at,
+      updated_at: book.updated_at
+    }
+  end
+end
